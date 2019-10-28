19ab0b4eec4ab7f467e5f98d7916eb4316844706
diurivj <diurivj@icloud.com>
2019-10-28 14:08:02 -0600

    done, css done, js done, mimified

--- main.js
+++ main.js
@@ -3,9 +3,8 @@
  * Segundo sample 185.8, 187.5
  * Tercer sample 15.4, 23.14
  * Cuarto sample 46, 53.73
- * Quinto sample 77.4, 87.07
+ * Quinto sample 77.4, 87.07 // 83.2
  ******************/
-
 let isPlaying;
 const player = new Tone.Player({
   url: './utils/trackye.[mp3|ogg]',
@@ -13,12 +12,10 @@ const player = new Tone.Player({
 }).toMaster();
 
 player.volume.value = 5;
-
 document.querySelector('tone-player').bind(player);
 document.querySelector('tone-play-toggle').bind(player);
 
 document.addEventListener('keydown', function(e) {
-  if (isPlaying) return;
   switch (e.keyCode) {
     case 88:
       return sample(0, 3.85, e.key);
@@ -29,7 +26,7 @@ document.addEventListener('keydown', function(e) {
     case 86:
       return sample(46, 53.73, e.key);
     case 66:
-      return sample(77.4, 87.07, e.key); //83.2
+      return sample(77.4, 87.07, e.key);
     default:
       return;
   }
@@ -42,11 +39,30 @@ document.addEventListener('keyup', function(e) {
   player.loopStart = 0;
 });
 
+document.querySelector('#ArrowUp').onclick = (e) => setControls(1, 'ArrowUp');
+document.querySelector('#ArrowDown').onclick = (e) => setControls(-1, 'ArrowDown');
+document.querySelector('#ArrowLeft').onclick = (e) => setControls(-0.1, 'ArrowLeft');
+document.querySelector('#ArrowRight').onclick = (e) => setControls(0.1, 'ArrowRight');
+
 function sample(start, end, key) {
+  if (isPlaying) return;
   const div = document.querySelector(`#${key}`);
+  div.classList.add('pressed');
   isPlaying = true;
   player.loopStart = start;
   player.loopEnd = end;
   player.start();
+}
+
+function setControls(level, key) {
+  const div = document.querySelector(`#${key}`);
+  if (level % 1 === 0) {
+    player.volume.value += level;
+  } else {
+    player.playbackRate += level;
+  }
   div.classList.add('pressed');
+  setTimeout(() => {
+    div.classList.remove('pressed');
+  }, 100);
 }
